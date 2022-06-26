4ef6ed5ed808d4fb28c6042f0b4ce238e1044455
Jason Thorpe <thorpej@me.com>
2022-06-26 14:26:07 -0700

    Convert the "INCREMENT AND DECREMENT 16 BIT REGISTERS" macros.

--- newsrc/macros.s
+++ newsrc/macros.s
@@ -29,3 +29,56 @@ PULS_ALL	macro
 PULS_ALLPC	macro
 		PULS	D,CC,X,Y,U,PC
 		endm
+
+;
+;**********************************************
+;
+;  INCREMENT AND DECREMENT 16 BIT REGISTERS
+;
+;**********************************************
+;
+INCX		macro
+		LEAX 1,X
+		endm
+
+DECX		macro
+		LEAX -1,X
+		endm
+
+INCY		macro
+		LEAY 1,Y
+		endm
+
+DECY		macro
+		LEAY -1,Y
+		endm
+
+INCU		macro
+		LEAU 1,U
+		endm
+
+DECU		macro
+		LEAU -1,U
+		endm
+
+INCS		macro
+		LEAS 1,S
+		endm
+
+DECS		macro
+		LEAS -1,S
+		endm
+
+INCD		macro
+		INCB
+		BNE	666F
+		INCA
+666
+		endm
+
+DECD		macro
+		TSTB
+		BNE	668F
+		DECA
+668		DECB
+		endm
